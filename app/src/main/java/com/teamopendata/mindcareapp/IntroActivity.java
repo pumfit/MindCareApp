a33e0a920f528a2f2a86a9556bc3662d3c183500
codekim97 <kimtaeu4616@gmail.com>
2021-11-07 15:55:47 +0900

    Intro화면 추가 ~ -김태우

---
+++ app/src/main/java/com/teamopendata/mindcareapp/IntroActivity.java
@@ -0,0 +1,35 @@
+package com.teamopendata.mindcareapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+    private final String TAG = MainActivity.class.getSimpleName();
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.intro);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override public void run() {
+                Intent intent = new Intent(IntroActivity.this,MainActivity.class);
+                startActivity(intent); finish();
+            }
+        },800);
+    }
+
+
+    @Override
+    protected  void onPause(){
+        super.onPause();
+        finish();
+    }
+}
