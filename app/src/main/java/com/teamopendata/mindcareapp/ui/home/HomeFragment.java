72a9da3abfcc6cb69c5884ec195bc64cb4a1ea5d
Oseong Kwon <oxojjjj@gmail.com>
2021-11-08 23:03:54 +0900

    feat: 홈 화면 ui 구현

--- app/src/main/java/com/teamopendata/mindcareapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/teamopendata/mindcareapp/ui/home/HomeFragment.java
@@ -1,69 +1,36 @@
 package com.teamopendata.mindcareapp.ui.home;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
-import com.teamopendata.mindcareapp.R;
-import com.teamopendata.mindcareapp.SelectActivity;
+import com.teamopendata.mindcareapp.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
+    private FragmentHomeBinding binding;
 
-    private HomeViewModel homeViewModel;
-    android.widget.Button back_select_button;
-    TextView tvKeyword;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentHomeBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
 
-    String stress,fear, insomnia;
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
-        View HomeView = inflater.inflate(R.layout.fragment_home, container, false);
-        back_select_button = HomeView.findViewById(R.id.back_select_button);
-        tvKeyword = HomeView.findViewById(R.id.tvKeyword);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        stress = "";
-        fear = "";
-        insomnia = "";
-        Intent intent = getActivity().getIntent();
-        Bundle bundle = intent.getExtras();
-        if(bundle.getString("stress") != null){
-            stress = bundle.getString("stress");
-        }
-        if(bundle.getString("fear") != null){
-            fear = bundle.getString("fear");
-        }
-        if(bundle.getString("insomnia") != null){
-            insomnia = bundle.getString("insomnia");
-        }
+    }
 
-
-
-        String keywords = "";
-
-        keywords = "keywords = "+ stress +","+ fear +","+ insomnia;
-
-        tvKeyword.setText(keywords);
-
-        //!-- keyword 다시 설정하는 화면으로 이동
-        back_select_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Intent intent = new Intent(getActivity(), SelectActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        return HomeView;
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
