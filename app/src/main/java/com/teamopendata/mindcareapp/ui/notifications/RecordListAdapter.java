17b28b3a6167a22393d47ec20fb89a2bd53aa186
joo98 <slew61@naver.com>
2021-11-08 13:39:26 +0900

    record list update (1)

--- app/src/main/java/com/teamopendata/mindcareapp/ui/notifications/RecordListAdapter.java
+++ app/src/main/java/com/teamopendata/mindcareapp/ui/notifications/RecordListAdapter.java
@@ -1,5 +1,8 @@
 package com.teamopendata.mindcareapp.ui.notifications;
 
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -11,36 +14,44 @@ import com.teamopendata.mindcareapp.R;
 
 import java.util.ArrayList;
 
-public class RecordListAdapter extends RecyclerView.Adapter<RecordListAdapter.ViewHolder> {
+public class RecordListAdapter extends RecyclerView.Adapter<RecordListAdapter.RecordViewHolder> {
 
     private ArrayList<String> arrayList;
 
     public RecordListAdapter(ArrayList<String> list) { arrayList = list; }
 
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public RecordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View view  = inflater.inflate(R.layout.record_yourlist_item,parent,false);
+        RecordViewHolder recordViewHolder = new RecordViewHolder(view);
+
+        return recordViewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull RecordListAdapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull RecordViewHolder holder, int position) {
+        String text = arrayList.get(position);
+        Log.d("list",arrayList.get(position));
+        holder.textView1.setText(text);
+        holder.textView2.setText("처 방");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return arrayList.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class RecordViewHolder extends RecyclerView.ViewHolder {
 
         public TextView textView1;
         public TextView textView2;
 
-        public ViewHolder(View itemView) {
+        public RecordViewHolder(View itemView) {
             super(itemView);
-            textView1 = itemView.findViewById(R.id.textTitle);
-            textView2 = itemView.findViewById(R.id.textType);
+            textView1 = itemView.findViewById(R.id.textDate);
+            textView2 = itemView.findViewById(R.id.textRecordType);
         }
 
     }
