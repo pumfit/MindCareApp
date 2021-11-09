9231416b20b017cbc1bbaae353bd2c6e7119a147
Oseong Kwon <oxojjjj@gmail.com>
2021-11-09 11:24:14 +0900

    Home 구현 (#2)
    * feat: 리사이클러뷰, 리스트뷰 추가
    
    * feat: 리사이클러뷰 추가
    
    * feat: home ui 구현 완료(dummy data)

--- app/src/main/java/com/teamopendata/mindcareapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/teamopendata/mindcareapp/ui/home/HomeFragment.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.teamopendata.mindcareapp.R;
 import com.teamopendata.mindcareapp.databinding.FragmentHomeBinding;
+import com.teamopendata.mindcareapp.ui.home.model.TaskDTO;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
@@ -21,10 +26,32 @@ public class HomeFragment extends Fragment {
         View view = binding.getRoot();
         return view;
     }
-  
+
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        // dummy data
+        ArrayList<TaskDTO> dummyTasks = new ArrayList<>();
+        dummyTasks.add(new TaskDTO("오늘 병원 가기"));
+        dummyTasks.add(new TaskDTO("1시간 명상 하기"));
+        dummyTasks.add(new TaskDTO("즐거운 생각 하기"));
+        dummyTasks.add(new TaskDTO("길게 적으면 글자가 짤려요@@@@@@@@@@@@"));
+
+        ArrayList<String> dummyKeywords = new ArrayList<>();
+        dummyKeywords.add("불면");
+        dummyKeywords.add("스트레스");
+        dummyKeywords.add("외래,입원(입소)");
+        dummyKeywords.add("공포증");
+
+        // listView
+        binding.lvKeywords
+                .setAdapter(new ArrayAdapter<>(requireContext(), R.layout.item_home_keywords, dummyKeywords));
+
+        // recyclerView
+        TodayTaskAdapter adapter = new TodayTaskAdapter(dummyTasks);
+        binding.rvTodayTask.setAdapter(adapter);
+        // binding.rvTodayTask.setLayoutManager(new LinearLayoutManager(requireContext()));
     }
 
     @Override
