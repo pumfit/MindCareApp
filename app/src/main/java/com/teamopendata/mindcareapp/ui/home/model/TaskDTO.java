a7a19fb94d26485fe1d2fab1f723069ba9ec3426
Oseong Kwon <oxojjjj@gmail.com>
2021-11-09 11:02:25 +0900

    feat: home ui 구현 완료(dummy data)

--- app/src/main/java/com/teamopendata/mindcareapp/ui/home/model/TaskDTO.java
+++ app/src/main/java/com/teamopendata/mindcareapp/ui/home/model/TaskDTO.java
@@ -1,2 +1,40 @@
-package com.teamopendata.mindcareapp.ui.home.model;public class TaskDTO {
+package com.teamopendata.mindcareapp.ui.home.model;
+
+public class TaskDTO {
+    private String contents;
+    private boolean completed;
+
+    public TaskDTO(String contents, boolean completed) {
+        this.contents = contents;
+        this.completed = completed;
+    }
+
+    public TaskDTO(String contents) {
+        this.contents = contents;
+        completed = false;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskDTO{" +
+                "contents='" + contents + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
 }
