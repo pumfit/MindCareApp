7c9da77c0a1d0ca4f28fa0c718d7289cdd9e819c
joo98 <slew61@naver.com>
2021-11-22 13:50:27 +0900

    mindChargeDB 수정

--- app/src/main/java/com/teamopendata/mindcareapp/mindChargeDB.java
+++ app/src/main/java/com/teamopendata/mindcareapp/mindChargeDB.java
@@ -1,24 +1,54 @@
 package com.teamopendata.mindcareapp;
 
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+
+import com.teamopendata.mindcareapp.ui.map.MedicalInstitution;
+import com.teamopendata.mindcareapp.ui.map.MedicalInstitutionDao;
 
 @Database(entities = {MedicalInstitution.class}, version = 1)
 public class mindChargeDB extends RoomDatabase {
 
     private static mindChargeDB INSTANCE = null;
 
-    public abstract MedicalInstitutionDao medicalInstitutionDao();
+    public MedicalInstitutionDao medicalInstitutionDao() {
+        return null;
+    }
 
 
-    public static MusicDB getInstance(Context context) {
+    public static mindChargeDB getInstance(Context context) {
         if (INSTANCE == null) {
             INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
-                    MedicalInstitution.class, "medicalinstitution.db").build();
+                    mindChargeDB.class, "medicalinstitution.db").build();
         }
         return INSTANCE;
     }
 
     public static void destroyInstance() {
         INSTANCE = null;
+    }
+
+    @NonNull
+    @Override
+    protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration config) {
+        return null;
+    }
+
+    @NonNull
+    @Override
+    protected InvalidationTracker createInvalidationTracker() {
+        return null;
+    }
+
+    @Override
+    public void clearAllTables() {
+
     }
 }
